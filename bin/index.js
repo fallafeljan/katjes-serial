1eda8483b62b65407205bd837f4a91284b315faf
jk <fallafeljan@users.noreply.github.com>
2017-02-03 20:42:59 +0100

    serial fun 🏀

--- bin/index.js
+++ bin/index.js
@@ -22,15 +22,51 @@ catch (err) {
   process.exit(1)
 }
 
-const path = 'socket.io/?EIO=3&transport=websocket'
-const ws = new WebSocket(`ws://${args.host}/${path}`)
-const socket = new SIOWrapper(ws)
+const {path, host} = args
+console.log(`Connecting to device ${path} and ${host}`)
 
-socket.on('connect', () => {
-  socket.emit('handshake', {
-    identity: 'cli'
-  })
-
-  socket.on('handshake', ({response}) =>
-    socket.disconnect())
+var port = new SerialPort(path, {
+  baudRate: 9600
 })
+
+port.on('open', () => {
+  port.write('main screen turn on', err => {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+
+    createSocket(host)
+  })
+})
+
+port.on('error', err => {
+  console.log('Error: ', err.message)
+})
+
+port.on('data', buffer => {
+  if (!Buffer.isBuffer(buffer)) {
+    buffer = new Buffer(buffer)
+  }
+
+  const data = buffer.toString()
+  console.log(`Data (${typeof data}):`, data)
+
+  if (data === 'OK') {
+    port.write('OK')
+  }
+})
+
+function createSocket(host) {
+  const endpoint = 'socket.io/?EIO=3&transport=websocket'
+  const ws = new WebSocket(`ws://${host}/${endpoint}`)
+  const socket = new SIOWrapper(ws)
+
+  socket.on('connect', () => {
+    socket.emit('handshake', {
+      identity: 'cli'
+    })
+
+    socket.on('handshake', ({response}) =>
+      socket.disconnect())
+  })
+}
